af9376ee155f668d6e926a9765d5d4d60dd789c1
tezz-io <matblockgreninja@gmail.com>
2020-09-12 20:34:46 +0530

    Initialize project using Create React App

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCFLmEscV1ddiP7eBiKMZJdbsBs7QSSui4",
+    authDomain: "toktik-a0f5c.firebaseapp.com",
+    databaseURL: "https://toktik-a0f5c.firebaseio.com",
+    projectId: "toktik-a0f5c",
+    storageBucket: "toktik-a0f5c.appspot.com",
+    messagingSenderId: "987504241542",
+    appId: "1:987504241542:web:266a27e8ec8810fd9a9a07",
+    measurementId: "G-CHRYZ976JG"
+  };
